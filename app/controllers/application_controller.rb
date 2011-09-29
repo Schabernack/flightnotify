8738bd6b7852d035f9f3f56e2650c0851f6da0f1
Christian Horstmann <reinerzufall4u@gmail.com>
2011-09-29 10:24:55 +0200

    suche anhand von Eingabe und Auswertung
    Sucheeingabe unter /application/index
    Ergebnis unter /application/search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,29 @@ class ApplicationController < ActionController::Base
   require "bundler/setup"
   require "nokogiri"
   require "open-uri"
-
+  
   def index
-    @response = Nokogiri::HTML(open('http://www.google.com/search?q=x3+2312'))
-
-    @bla = []
-    @response.css('.rbt').each do |foo|
-      @bla << foo.content
-    end
+    
   end
+ 
+  def search
+    @response = Nokogiri::HTML(open('http://www.google.com/search?q='+params[:query].gsub(/ /,'')))
+
+    @bla =  @response.at_css("title").text
+    @response.css('.rbt').each do |foo|
+      
+      @flight = foo.at_css('b').text                               
+      @from = foo.at_css("tr:nth-child(3) td:nth-child(2)") .text
+      @to = foo.at_css("tr:nth-child(6) td:nth-child(2)").text
+      
+      
+      @departure = foo.at_css("tr:nth-child(2) td:nth-child(3)").text
+      @plannedDeparture = foo.at_css("tr:nth-child(2) td:nth-child(4)").text
+      
+      @arrival = foo.at_css("tr:nth-child(5) td:nth-child(3)").text
+      @plannedArrival = foo.at_css("tr:nth-child(5) td:nth-child(4)").text
+    end
+    
+  end
+  
 end
